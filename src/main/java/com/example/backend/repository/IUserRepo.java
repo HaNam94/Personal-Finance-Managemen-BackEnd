65efc2577564d0e5f938cd9f00f4610e7766548f
vansy61 <49103390+vansy61@users.noreply.github.com>
2024-08-06 15:37:43 +0700

    add api get user

--- src/main/java/com/example/backend/repository/IUserRepo.java
+++ src/main/java/com/example/backend/repository/IUserRepo.java
@@ -1,5 +1,6 @@
 package com.example.backend.repository;
 
+import com.example.backend.dto.UserDto;
 import com.example.backend.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,5 @@ public interface IUserRepo extends JpaRepository<User, Long> {
     Optional<User> findUserByEmail(String email);
     Optional<User> findUserByPhone(String phone);
     Optional<User> findUserByEmailAndPassword(String email, String password);
+    Optional<UserDto> findUserById(Long id);
 }
