8e00b0526f72148cac2e54e1605b09a1a6c23a7c
giakiet2309 <135315001+giakiet2309@users.noreply.github.com>
2024-08-07 10:25:24 +0700

    add reset password

--- src/main/java/com/example/backend/repository/IUserRepo.java
+++ src/main/java/com/example/backend/repository/IUserRepo.java
@@ -3,13 +3,15 @@ package com.example.backend.repository;
 import com.example.backend.dto.UserDto;
 import com.example.backend.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
+@Repository
 public interface IUserRepo extends JpaRepository<User, Long> {
     Optional<User> findUserByEmail(String email);
     Optional<User> findUserByPhone(String phone);
     Optional<User> findUserByEmailAndPassword(String email, String password);
+    User findByResetToken(String token);
     Optional<UserDto> findByEmail(String email);
 }
