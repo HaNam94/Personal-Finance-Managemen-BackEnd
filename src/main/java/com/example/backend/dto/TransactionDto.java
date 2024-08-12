0d37459ad38b636c86ec65d8065808c88cc47b5a
HaNam94 <haduynam.94@gmail.com>
2024-08-12 17:24:42 +0700

    fix bug

--- src/main/java/com/example/backend/dto/TransactionDto.java
+++ src/main/java/com/example/backend/dto/TransactionDto.java
@@ -15,8 +15,7 @@ import java.time.LocalDate;
 @Builder
 public class TransactionDto {
     private Long id;
-    @NotNull(message = ValidationMessage.NOT_NULL)
-    private String transactionType;
+
     private String note;
     @Min(value = 0)
     private BigDecimal amount;
